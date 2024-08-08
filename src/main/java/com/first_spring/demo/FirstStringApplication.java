0e5c097dcd22369047949a02d163964a019cf0dc
Adam - The Developer <adam@sala.co>
2024-08-08 18:55:22 +0700

    init first spring server

---
+++ src/main/java/com/first_spring/demo/FirstStringApplication.java
@@ -0,0 +1,13 @@
+package com.first_spring.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstStringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstStringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/first_spring/demo/FirstStringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.first_spring.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstStringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
