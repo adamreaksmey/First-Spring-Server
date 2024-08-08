d6c291096b54b724e6993c31922ed86fcdbdbc73
adamreaksmey <adaminiature@gmail.com>
2024-08-08 18:57:20 +0700

    testing commit

--- src/main/java/com/first_spring/demo/FirstStringApplication.java
+++ src/main/java/com/first_spring/demo/FirstStringApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class FirstStringApplication {
 
 	public static void main(String[] args) {
+		// Testing commit
 		SpringApplication.run(FirstStringApplication.class, args);
 	}
 
