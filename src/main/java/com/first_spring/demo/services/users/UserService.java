b60f852bb6d453be3496f4ad9032aaaa03007a09
adamreaksmey <adaminiature@gmail.com>
2024-08-11 18:13:57 +0700

    init utils

---
+++ src/main/java/com/first_spring/demo/services/users/UserService.java
@@ -0,0 +1,31 @@
+package com.first_spring.demo.services.users;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.first_spring.demo.entities.users.User;
+import com.first_spring.demo.repositories.UserRepository;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    // Create or Update a User
+    public User saveUser(User user) {
+        return userRepository.save(user);
+    }
+
+    // Get a User by ID
+    public Optional<User> getUserById(Long id) {
+        return userRepository.findById(id);
+    }
+
+    // Get all Users
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+}
