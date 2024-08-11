f4a188cb4834e3451c6022122a4354e64f549721
adamreaksmey <adaminiature@gmail.com>
2024-08-11 15:41:55 +0700

    init repository

---
+++ src/main/java/com/first_spring/demo/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.first_spring.demo.repositories;
+
+import com.first_spring.demo.entities.users.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
