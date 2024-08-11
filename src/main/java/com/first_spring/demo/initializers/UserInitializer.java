2195a7e989c5b91dff323a89a9783a793e5e1b80
adamreaksmey <adaminiature@gmail.com>
2024-08-11 15:48:37 +0700

    added initializer

---
+++ src/main/java/com/first_spring/demo/initializers/UserInitializer.java
@@ -0,0 +1,18 @@
+package com.first_spring.demo.initializers;
+
+import com.first_spring.demo.entities.users.User;
+import com.first_spring.demo.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+public class UserInitializer implements CommandLineRunner {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        userRepository.save(new User("John Doe", "john@example.com"));
+        userRepository.save(new User("Jane Doe", "jane@example.com"));
+    }
+}
