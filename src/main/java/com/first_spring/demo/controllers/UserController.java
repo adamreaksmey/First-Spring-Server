14b072d743b801f3fa088ac237a5553d6789d8c0
adamreaksmey <adaminiature@gmail.com>
2024-08-11 18:31:20 +0700

    added some mappings example

--- src/main/java/com/first_spring/demo/controllers/UserController.java
+++ src/main/java/com/first_spring/demo/controllers/UserController.java
@@ -1,34 +1,47 @@
 package com.first_spring.demo.controllers;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.first_spring.demo.entities.users.User;
-import com.first_spring.demo.repositories.UserRepository;
+import com.first_spring.demo.services.users.UserService;
 
 @RestController
+@RequestMapping("/api/users")
 public class UserController {
     @Autowired
-    private UserRepository userRepository;
+    private UserService userService;
 
-    @GetMapping("/users")
-    public List<User> getFirstUserName() {
-        List<User> users = userRepository.findAll();
-        return users;
+    // Create a new User
+    @PostMapping
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        User savedUser = userService.saveUser(user);
+        return new ResponseEntity<>(savedUser, HttpStatus.CREATED);
+    }
 
-        // // Check if the list is not empty
-        // if (!users.isEmpty()) {
-        // // Get the first user in the list
-        // User oneUser = users.get(0);
-        // // Return the first user's name
-        // return oneUser.getName();
-        // }
+    // Get a User by ID
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable Long id) {
+        Optional<User> user = userService.getUserById(id);
+        return user.map(ResponseEntity::ok)
+                .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
 
-        // System.out.println("No users found.");
-        // return "No user found";
+    // Get all Users
+    @GetMapping
+    public ResponseEntity<List<User>> getAllUsers() {
+        List<User> users = userService.getAllUsers();
+        return new ResponseEntity<>(users, HttpStatus.OK);
     }
 
 }
