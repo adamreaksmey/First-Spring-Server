db1f002c62d845d6cc7847f1f3209398ebab0f0a
adamreaksmey <adaminiature@gmail.com>
2024-08-11 17:08:26 +0700

    moved testing controller

--- src/main/java/com/first_spring/demo/controllers/TestingController.java
+++ src/main/java/com/first_spring/demo/controllers/TestingController.java
@@ -1,4 +1,4 @@
-package com.first_spring.demo;
+package com.first_spring.demo.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
